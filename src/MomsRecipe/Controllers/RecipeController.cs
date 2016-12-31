09de23dc4614875ee42e36000f334f1c20ed7505
Daniel Bider <bider.daniel@gmail.com>
2016-12-31 12:34:12 +0100

    Added npm and stuff

---
+++ src/MomsRecipe/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MomsRecipe.Controllers
+{
+    public class RecipeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
