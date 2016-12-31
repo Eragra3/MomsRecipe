f1c0c99308dbfeabea4f502c42c77542bf01b8b8
Daniel Bider <bider.daniel@gmail.com>
2016-12-31 13:01:14 +0100

    Added some more files

---
+++ src/MomsRecipe/Client/Scripts/app.module.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('momsRecipe', [
+        // Angular modules 
+        'ngAnimate',
+
+        // Custom modules 
+        'momsRecipe.common',
+        'momsRecipe.recipe',
+
+        // 3rd Party Modules
+        'ui.router'
+
+    ]);
+})();
