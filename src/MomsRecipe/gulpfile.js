68833643b3fc989c3cd8590f600ecc545b003ae8
Daniel Bider <bider.daniel@gmail.com>
2016-12-31 14:17:32 +0100

    Added tyles inject

--- src/MomsRecipe/gulpfile.js
+++ src/MomsRecipe/gulpfile.js
@@ -2,25 +2,31 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var inject = require('gulp-inject');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'styles', 'inject']);
+
+/*
+ * SCRIPTS
+ */
 
 gulp.task('scripts', [
     'scripts.packages',
     'scripts.app.modules',
-    'scripts.app',
-    'scripts.inject'
+    'scripts.app'
 ]);
 
 gulp.task('scripts.app', function () {
-    gulp.src(['client/**/*.js', '!client/**/*.module.js'])
+    gulp.src([
+        'client/scripts/**/*.js',
+        '!client/scripts/**/*.module.js'])
         .pipe(uglify())
         .pipe(concat('app.js'))
         .pipe(gulp.dest('wwwroot/scripts'));
 });
 
 gulp.task('scripts.app.modules', function () {
-    gulp.src('client/**/*.module.js')
+    gulp.src('client/scripts/**/*.module.js')
         .pipe(uglify())
         .pipe(concat('app.modules.js'))
         .pipe(gulp.dest('wwwroot/scripts'));
@@ -37,7 +43,38 @@ gulp.task('scripts.packages', function () {
         .pipe(gulp.dest('wwwroot/scripts'));
 });
 
-gulp.task('scripts.inject', function () {
+/*
+ * STYLES
+ */
+
+gulp.task('styles', [
+    'styles.packages',
+    'styles.app',
+]);
+
+gulp.task('styles.packages', function () {
+    gulp.src([
+        'node_modules\bootstrap\dist\css\bootstrap.css',
+    ])
+        .pipe(minifyCss())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('wwwroot/styles'));
+});
+
+gulp.task('styles.app', function () {
+    gulp.src([
+        'client/styles/**/*.css',
+    ])
+        .pipe(minifyCss())
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('wwwroot/styles'));
+});
+
+/*
+ * INJECT
+ */
+
+gulp.task('inject', function () {
     var target = gulp.src('Views/App.cshtml');
 
     var sources = gulp.src([
@@ -51,4 +88,3 @@ gulp.task('scripts.inject', function () {
     return target.pipe(inject(sources))
       .pipe(gulp.dest('Views/'));
 });
-
